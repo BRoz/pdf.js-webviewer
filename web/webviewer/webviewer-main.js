d9de2571a67a31449fd009af7205587731b5f2f4
Caleb James DeLisle <cjd@hyperboria.ca>
2013-11-07 16:37:24 +0100

    bug fixing

--- web/webviewer/webviewer-main.js
+++ web/webviewer/webviewer-main.js
@@ -1,11 +1,10 @@
 /*global window, jQuery, rJS */
-(function (window, $, rJS) {
+;(function (window, rJS) {
   "use strict";
-
-  rJS(window).declareMethod('setContent', function (url) {
-    $(function() {
+  rJS(window).ready(function () {
+    rJS(window).declareMethod('setContent', function (url) {
       PDFView.open(url, 0);
     });
   });
 
-}(window, jQuery, rJS))
+}(window, rJS));
